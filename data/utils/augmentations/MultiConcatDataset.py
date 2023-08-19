221d06de4dc4e6d5683ab165c6e34bc5a0fc78ed
Martin Otterbein <Martin.Otterbein@ofgd.de>
2023-08-19 08:39:58 +0000

    Fixed MulticoncatDataset

--- data/utils/augmentations/MultiConcatDataset.py
+++ data/utils/augmentations/MultiConcatDataset.py
@@ -1,17 +1,3 @@
-class AugSet(Dataset):
-    def __init__(self, subset, transform=None):
-        self.subset = subset
-        self.transform = transform
-        
-    def __getitem__(self, index):
-        x, y = self.subset[index]
-        if self.transform:
-            x = self.transform(x)
-        return x, y
-        
-    def __len__(self):
-        return len(self.subset)
-
 import bisect
 import warnings
 import torch
