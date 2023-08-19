ee35b29463de4481ce3d8e2b3332ae676bf95ced
Martin <Martin.Otterbein@ofgd.de>
2023-08-19 10:31:58 +0200

    Fixed distribution of ConcatDataset indexes among subsets

--- data/utils/augmentations/MultiConcatDataset.py
+++ data/utils/augmentations/MultiConcatDataset.py
@@ -1,3 +1,17 @@
+class AugSet(Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+        
+    def __len__(self):
+        return len(self.subset)
+
 import bisect
 import warnings
 import torch
@@ -25,7 +39,7 @@ T_co = TypeVar('T_co', covariant=True)
 from torch.utils.data import Dataset, IterableDataset
 
 
-class MultiConcatDataset(Dataset[T_co]):
+class DultiConcatDataset(Dataset[T_co]):
     r"""Dataset as a concatenation of multiple datasets.
 
     This class is useful to assemble different existing datasets.
@@ -57,64 +71,35 @@ class MultiConcatDataset(Dataset[T_co]):
         return self.cumulative_sizes[-1]
 
     def __getitem__(self, idx):
-        # if idx < 0:
-        #     if -idx > len(self):
-        #         raise ValueError("absolute value of index should not exceed dataset length")
-        #     idx = len(self) + i
-        # print(idx.shape)
         dataset_idx = torch.as_tensor([bisect.bisect_right(self.cumulative_sizes, id_) for id_ in idx])
         sample_idx = torch.as_tensor([i_d if d_id == 0 else i_d - self.cumulative_sizes[d_id - 1]  for i_d, d_id in zip(idx, dataset_idx)])
         d2_indices =torch.stack((dataset_idx, sample_idx, torch.as_tensor(idx), torch.arange(0, len(idx))))
-        # print(d2_indices.shape)
-        # sort_mapping = a[0,:].argsort()
-        # sorted_indices = d2_indices[sort_mapping]
 
         mapped_sample_indices = []
         targets = []
         inputs = []
+        # print(f"Retrieving samples from datasets with ids {torch.unique(dataset_idx)}")
         for dataset_id in torch.unique(dataset_idx):
             dataset_samples = d2_indices[0]==dataset_id
-            # print(len(dataset_samples.shape))
-            # print(d2_indices[:,dataset_samples].shape)
-            # print(d2_indices[:,dataset_samples][1].shape)
-            # sample_indices = d2_indices[:,dataset_samples][1]
-            # print(sample_indices.shape)
             sample_indices = d2_indices[:,dataset_samples][1]
+
             selected_data = self.datasets[dataset_id][sample_indices.tolist()]
             selected_ids = d2_indices[:,dataset_samples]
-            # print("Selected Id Shape:")
-            # print(selected_ids.shape)
-            mapped_sample_indices.append(selected_ids)
-            # print(inputs[0].shape)
-            
+
+            mapped_sample_indices.append(selected_ids)      
             inputs.append(selected_data[0])
             targets.append(selected_data[1])
 
-        # data = torch.as_tensor(data)
         tensor_targets = torch.concat(targets, dim=0)
         tensor_inputs = torch.concat(inputs, dim=0)
         tensor_indices_mapping = torch.concat(mapped_sample_indices, dim=1)
-
-        sorting_index = tensor_indices_mapping[3]
+        test_index = tensor_indices_mapping[3].argsort()
+        sorting_index = test_index
         sorted_indices = tensor_indices_mapping[:,sorting_index]
-
-        # print(torch.sum(sorted_indices == d2_indices))
-        # print(tensor_indices_mapping[2].shape)
-        # print(len(idx))
-        # print(d2_indices.shape)
-        # print(tensor_indices_mapping.shape)
 
         tensor_targets = tensor_targets[sorting_index]
         tensor_inputs = tensor_inputs[sorting_index]
 
-
-        # print(tensor_targets.shape)
-        # print(tensor_inputs.shape)
-
-        # if dataset_idx == 0:
-        #     sample_idx = idx
-        # else:
-        #     sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
         return (tensor_inputs, tensor_targets)
 
     @property
